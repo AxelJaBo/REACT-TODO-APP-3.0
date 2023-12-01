3e8f7be73b0d8c86aff8e09c420d2a41db090356
AxelJaBo <axel.910921@gmail.com>
2023-11-30 20:40:58 -0800

    TODO-APP-3.0 start

---
+++ src/App/useLocalStorage.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+
+function useLocalStorage(itemName, initialValue) {
+    const [item, setItem] = React.useState(initialValue);
+    const [loading, setLoading] = React.useState(true);
+    const [error, setError] = React.useState(false);
+    const [sincronizedItem, setSincronizedItem] = React.useState(true);
+
+    React.useEffect(()=> {    
+        setTimeout(()=> {
+            try{
+                const localStorageItem = localStorage.getItem(itemName);
+                let parsedItem;
+                if (!localStorageItem) {
+                    localStorage.setItem(itemName, JSON.stringify(initialValue));
+                    parsedItem = initialValue;
+                } else {
+                    parsedItem = JSON.parse(localStorageItem);
+                    setItem(parsedItem);
+                }
+                setLoading(false);
+                setSincronizedItem(true);
+            }catch(error){
+                setLoading(false);
+                setError(true);
+            }
+        }, 2000);
+    }, [sincronizedItem]);
+
+    const saveItem = (newItem) => {
+        localStorage.setItem('TODOS_V1', JSON.stringify(newItem));
+        setItem(newItem);
+    };
+
+    const sincronizeItem = () => {
+        setLoading(true);
+        setSincronizedItem(false);
+    }
+
+    return {item, saveItem, error, loading, sincronizeItem};
+}
+
+export { useLocalStorage };
